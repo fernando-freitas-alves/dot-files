faa64c9cba7e9f0dfe7080ec821a173d291dece1
fernando-freitas-alves <fernando.freitas.alves@gmail.com>
2020-06-18 20:20:09 -0300

    feat: added i3-float python script

--- polybar/scripts/dist-updates-check-new-window.sh
+++ polybar/scripts/dist-updates-check-new-window.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-i3-sensible-terminal \
-    bash -ic \
-    'echo "Upgrading dist packages"; sudo apt upgrade; read -p "Press any key to exit..."; exit'
+# i3-sensible-terminal \
+#     bash -itc \
+#     'echo "Openning aptitude"; aptitude -i'
+#     # 'echo "Upgrading dist packages"; sudo apt upgrade; read -p "Press any key to exit..."; exit'
 
+i3-float -s -n kitty -W 1200 -H 900 kitty aptitude -i
