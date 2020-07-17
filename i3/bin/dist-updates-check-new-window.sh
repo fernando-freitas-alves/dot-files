4155800f46e09a42dea9de1b5670efcf8bacc899
fernando-freitas-alves <fernando.freitas.alves@gmail.com>
2020-07-17 19:00:10 -0300

    fix: solved an issue on calling new i3-float

--- i3/bin/dist-updates-check-new-window.sh
+++ i3/bin/dist-updates-check-new-window.sh
@@ -5,4 +5,12 @@
 #     'echo "Openning aptitude"; aptitude -i'
 #     # 'echo "Upgrading dist packages"; sudo apt upgrade; read -p "Press any key to exit..."; exit'
 
-i3-float -s -n kitty -W 1200 -H 900 kitty aptitude -i
+# i3-float -s -n kitty -W 1200 -H 900 kitty aptitude -i
+
+BASEDIR=$(dirname $0)
+CLASS=$(basename $(readlink -e $(which x-terminal-emulator)))
+# CMD="x-terminal-emulator sudo -E TERMINFO=\"$TERMINFO\" aptitude -i"
+CMD="x-terminal-emulator aptitude -i"
+# CMD="$BASEDIR/dist-updates.sh"
+i3-float -s -n $CLASS -W 1200 -H 900 $CMD
+# echo TERMINFO="$TERMINFO" i3-float -s -n $CLASS -W 1200 -H 900 $CMD
